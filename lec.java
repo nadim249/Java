fdb6007ba5afde741905258009afc304ee4acdff
Khalid Mahmud Nadim <134575357+nadim249@users.noreply.github.com>
2023-08-26 10:58:34 +0600

    first commit

--- lec.java
+++ lec.java
@@ -1,4 +1,9 @@
 public class lec {
-    public static void main(String[] args){
- }
+    public static void main(String[] args) {
+        int age1 =30;
+        float age = 5.555f;
+        long codem =2432413523l;
+        System.out.println(age1 );
+    }
 }
+
